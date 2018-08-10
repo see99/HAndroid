8c0706b3ef682deddccd8225f4b29fe6ccfa9bb3
buend <xintian13@outlook.com>
2018-08-10 17:13:08 +0800

    SPTools

--- toolslibrary/src/main/java/top/buend/toolslibrary/DeviceMsgTools.java
+++ toolslibrary/src/main/java/top/buend/toolslibrary/DeviceMsgTools.java
@@ -8,9 +8,7 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.annotation.RequiresApi;
 import android.support.annotation.RequiresPermission;
-import android.support.v4.app.ActivityCompat;
 import android.telephony.TelephonyManager;
 
 import static android.Manifest.permission.ACCESS_WIFI_STATE;
